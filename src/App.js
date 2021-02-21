ded869ea2b811d750d2893fa9b81391f2e9d881f
raaynaldo <raaynaldo@hotmail.com>
2021-02-20 19:40:25 -0500

    .env and setup react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
-
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-     
+      <div>
+        <nav>
+          <ul className="nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/users">User</Link>
+            </li>
+            <li>
+              <Link to="/posts">Post</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/posts">
+            <div>post</div>
+          </Route>
+          <Route path="/users">
+            <div>users</div>
+          </Route>
+          <Route path="/">
+            <div>home</div>
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
